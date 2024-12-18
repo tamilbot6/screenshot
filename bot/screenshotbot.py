cee51b5542b691476a0cb06fa02a4e2713d99445
tamilbot6 <ghostcm83@gmail.com>
2024-12-19 00:24:26 +0530

    Update screenshotbot.py

--- bot/screenshotbot.py
+++ bot/screenshotbot.py
@@ -20,7 +20,7 @@ log = logging.getLogger(__name__)
 class ScreenShotBot(Client):
     def __init__(self):
         super().__init__(
-            session_name="my_bot",
+            name=Config.SESSION_NAME,
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
